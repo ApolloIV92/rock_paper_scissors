c4703608a54bc0d8c97fcada409495606e10768c
ApolloIV92 <rmorzel@gmail.com>
2022-06-04 22:34:07 -0500

    Add finished game logic and error checking (ensure player makes an appropriate selection)

--- javascript.js
+++ javascript.js
@@ -9,10 +9,32 @@ function randomChoice() {
 
 function playRound(playerChoice, cpuChoice) {
     if (playerChoice === null) console.log("Game aborted");
-    else {
-        console.log(`You chose: ${playerChoice}`);
-        console.log(`Your opponent chose: ${cpuChoice}`)
+    else if (capitalizeInput(playerChoice) !== 'Rock' && capitalizeInput(playerChoice) !== "Scissors" && capitalizeInput(playerChoice) !== "Paper") {
+        let playerSelection = window.prompt("Please enter 'Rock', 'Paper', or 'Scissors'. Choose your move!");
+
+        playRound(playerSelection, cpuChoice);
     }
+    else {
+        let sanitizedPlayerChoice = capitalizeInput(playerChoice);
+        console.log(`You chose: ${sanitizedPlayerChoice}`);
+        console.log(`Your opponent chose: ${cpuChoice}`);
+
+        if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Scissors" || sanitizedPlayerChoice === 
+        "Scissors" && cpuChoice === "Paper" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Rock") {
+            console.log("You win!");
+        } else if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Rock" || sanitizedPlayerChoice === 
+        "Scissors" && cpuChoice === "Scissors" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Paper") {
+            console.log("You tied!");
+        } else if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Paper" || sanitizedPlayerChoice === 
+        "Scissors" && cpuChoice === "Rock" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Scissors") {
+            console.log("You lost!");
+    }
+}
+}
+
+function capitalizeInput(input) {
+    let sanitizedPlayerChoice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+    return sanitizedPlayerChoice;
 }
 
 playRound(playerSelection, cpuSelection);
