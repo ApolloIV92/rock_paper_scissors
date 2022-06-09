db12e5ac30489621d6a5a431d8a9697649c3ea2f
ApolloIV92 <rmorzel@gmail.com>
2022-06-09 14:13:48 -0500

    Refactor game logic and display results when move is chosen

--- javascript.js
+++ javascript.js
@@ -1,56 +1,52 @@
-let throws = ['Paper', "Scissors", "Rock"];
+let throws = ['rock', 'paper', 'scissors'];
 let outcome;
 let wins = 0;
 let losses = 0; 
 let ties = 0;
+let rounds = 0;
+
+const headerText = document.querySelector(".headerContainer");
+const score = document.createElement("h2")
 
 function randomChoice() {
     let choice = throws[Math.floor(Math.random() * 3)];
     return choice;
 }
 
-function playRound(playerChoice, cpuChoice) {
-    if (playerChoice === null) console.log("Game aborted");
-    else if (capitalizeInput(playerChoice) !== 'Rock' && capitalizeInput(playerChoice) !== "Scissors" && capitalizeInput(playerChoice) !== "Paper") {
-        let playerSelection = window.prompt("Please enter 'Rock', 'Paper', or 'Scissors'. Choose your move!");
-        playRound(playerSelection, cpuChoice);
-    }
-    else {
-        let sanitizedPlayerChoice = capitalizeInput(playerChoice);
-        console.log(`You chose: ${sanitizedPlayerChoice}`);
-        console.log(`Your opponent chose: ${cpuChoice}`);
-
-        if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Scissors" || sanitizedPlayerChoice === 
-        "Scissors" && cpuChoice === "Paper" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Rock") {
-            wins++
-            outcome = `You win! Score: Wins ${wins} Losses ${losses} Ties ${ties}`;
-            return outcome;
-        } else if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Rock" || sanitizedPlayerChoice === 
-        "Scissors" && cpuChoice === "Scissors" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Paper") {
-            ties++;
-            outcome = `You tied! Score: Wins ${wins} Losses ${losses} Ties ${ties}`;
-            return outcome;
-        } else if (sanitizedPlayerChoice === "Rock" && cpuChoice === "Paper" || sanitizedPlayerChoice === 
-        "Scissors" && cpuChoice === "Rock" || sanitizedPlayerChoice === "Paper" && cpuChoice === "Scissors") {
+function playRound(playerChoice) {
+    const cpuChoice = randomChoice();
+    if ((playerChoice === 'rock' && cpuChoice === 'paper') || 
+        (playerChoice === 'paper' && cpuChoice === 'scissors') || 
+        (playerChoice === 'scissors' && cpuChoice === 'rock')) {
+            outcome = `You chose ${playerChoice} and your opponent chose ${cpuChoice}. You lose!`;
             losses++;
-            outcome = `You lost! Score: Wins ${wins} Losses ${losses} Ties ${ties}`;
-            return outcome;
+            rounds++;
+    } else if ((playerChoice === 'rock' && cpuChoice === 'scissors') || 
+               (playerChoice === 'paper' && cpuChoice === 'rock') || 
+               (playerChoice === 'scissors' && cpuChoice === 'paper')) {
+        outcome = `You chose ${playerChoice} and your opponent chose ${cpuChoice}. You win!`;
+        wins++;
+        rounds++;
+    } else if ((playerChoice === 'rock' && cpuChoice === 'scissors') || 
+               (playerChoice === 'paper' && cpuChoice === 'rock') || 
+               (playerChoice === 'scissors' && cpuChoice === 'paper')) {
+        outcome = `You chose ${playerChoice} and your opponent chose ${cpuChoice}. You tied!`;
+        ties++;
+        rounds++;
     }
-}
-}
-
-function capitalizeInput(input) {
-    let sanitizedPlayerChoice = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-    return sanitizedPlayerChoice;
+    showResult(outcome);
 }
 
-function game() {
-    for (let i = 0; i<5; i++) {
-        let playerSelection = prompt("Choose your move!");
-        let cpuSelection = randomChoice();
-        console.log(playRound(playerSelection, cpuSelection));
-    }
+function showResult(outcome) {
+    const result = document.createElement("p");
+    result.classList.add('resultsMessage');
+    result.textContent = outcome;
+    const showResults = document.querySelector(".results");
+    showResults.insertBefore(result, showResults.firstChild);
+
 }
 
-// game();
-
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.id))
+});
